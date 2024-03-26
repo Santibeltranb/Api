608525dfdb35ca8478a09330520dadf020067437
SantiBeltranB <Santibeltrxnb@gmail.com>
2024-03-25 19:04:33 -0500

    Api Completa

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,14 @@
+export const environment = {
+
+    firebase: {
+
+        projectId:"contacts-projects",
+        appId:"1:792512721675:web:51364b86da3ae7417fa80e",
+        storageBucket:"contacts-projects.appspot.com",
+        apiKey:"AIzaSyDAejqIzPU0i36Ygcf3pC-oX7F60yDFMz8",
+        authDomain:"contacts-projects.firebaseapp.com",
+        messagingSenderId:"792512721675"}
+
+
+};
+
